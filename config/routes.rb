64dcab990c8caf1fdf5f4b94869b7699264eea46
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-02-19 23:06:21 -0500

    4.3 Added orders controllers, index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products
   # resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'static_pages/index'
 
   get 'static_pages/about'
@@ -10,7 +13,8 @@ Rails.application.routes.draw do
   get 'products/new'
 
   #get 'static_pages/index'
-  root 'static_pages#index'
+  #root 'static_pages#landing_page'
+	root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
