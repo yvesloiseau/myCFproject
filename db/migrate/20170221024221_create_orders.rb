5b48ec6e5ae2ec9b556535a586ca03e66038ba1d
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-02-21 23:01:09 -0500

    4.4 Added the Orders model and relations with Products

---
+++ db/migrate/20170221024221_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :quantity
+      t.float :net_amount
+      t.float :taxes
+      t.float :total_amount
+    end
+    add_index :orders, :user_id
+    add_index :orders, :product_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217025807) do
+ActiveRecord::Schema.define(version: 20170221024221) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.float   "net_amount"
+    t.float   "taxes"
+    t.float   "total_amount"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
