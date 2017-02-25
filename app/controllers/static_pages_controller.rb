b19395d3b2a9e0e1aa84c83c467f260785aed56c
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-02-25 14:57:13 -0500

    4.5.1 Added new variables and adjusted font and sizes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,8 @@ class StaticPagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
 	@products = Product
+	@featuredBrand = "Taylor Made"
+	@brand = "TaylorMade"
   end
   
 end
