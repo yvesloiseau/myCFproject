469d9f9ca111634b933fc3d517fd9eeeea234a65
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-02-25 09:36:52 -0500

    4.5 Create landing page with carousel images and use render partial for views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
+	@products = Product
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
 
   get 'products/new'
 
-  #get 'static_pages/index'
-  #root 'static_pages#landing_page'
-	root 'static_pages#index'
+  # get 'static_pages/index'
+   root 'static_pages#landing_page'
+	# root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
