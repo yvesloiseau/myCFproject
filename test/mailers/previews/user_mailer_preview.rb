d1475f99b9ed3eeb420f8f311e5ab03d2b72a143
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-03-25 17:42:32 -0400

    5.3.3 Added styling to the contact_form following suggested sites

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 	def contact_form
-	    UserMailer.contact_form("john@example.com", "John", "Hello World!")
+	    UserMailer.contact_form("john@example.com", "John", "Hello, you site is very nice. You are doing a good job at Career Foundry. Keep up the good work ans you wil acheive your goals.")
 	end
 end
