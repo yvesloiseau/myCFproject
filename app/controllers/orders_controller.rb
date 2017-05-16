4446b583a962f6f9d872dd872c09ac0494cdc106
Yves Loiseaue <yves.loiseau@videotron.ca>
2017-05-16 07:56:53 -0400

    6.8.1 Installed the brakeman gem for vulnerability checks - first tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -72,6 +72,11 @@ class OrdersController < ApplicationController
     end
   end
 
+  #  Capturing cancan exception and displaying a flash error message
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
+
 private
 
 def set_order
